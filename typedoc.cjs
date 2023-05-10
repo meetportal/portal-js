6417de90ec255d1cce0dfa280c1a1fe634c688ab
Rob Taylor <roboncode@gmail.com>
2023-05-10 08:04:39 -0600

    updated links

--- typedoc.cjs
+++ typedoc.cjs
@@ -8,7 +8,7 @@ module.exports = {
     Github: 'https://github.com/meetportal/portal-js',
     Website: 'https://meetportal.com',
     'Developer Console': 'https://marketplace.meetportal.com/developer',
-    'Getting Started': '#',
+    'Getting Started': 'https://meetportal.com/developers/getting-started',
   },
   disableSources: true,
   // theme: 'just-the-docs',
